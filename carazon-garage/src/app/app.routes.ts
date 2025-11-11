f0377d0a244bdf2d308715437a277b9f9ff1e608
DaniBenedek <dbenedek06@gmail.com>
2025-11-11 13:06:10 +0100

    oldalak létrehozása home page fix

--- carazon-garage/src/app/app.routes.ts
+++ carazon-garage/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
-import { Szerviz } from './components/szerviz/szerviz';
+import { Szerviz } from './pages/szerviz/szerviz';
+import { Home } from './pages/home/home';
 
 export const routes: Routes = [
-  { path: 'Szerviz', component: Szerviz }
+  { path: 'Szerviz', component: Szerviz },
+  { path: '', component: Home }
 ];
