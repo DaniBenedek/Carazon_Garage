213bd46d9d9867ef9d13044c9305cd65c3154def
DaniBenedek <dbenedek06@gmail.com>
2025-11-11 10:16:58 +0100

    Routerlink alapjaiban

--- carazon-garage/src/app/app.routes.ts
+++ carazon-garage/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { Szerviz } from './components/szerviz/szerviz';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'Szerviz', component: Szerviz }
+];
