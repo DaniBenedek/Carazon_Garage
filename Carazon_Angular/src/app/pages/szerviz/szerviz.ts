f8384aae86cec4085f23d187ef35009a31850fdd
DaniBenedek <dbenedek06@gmail.com>
2025-11-13 11:06:16 +0100

    nagy a baj

---
+++ Carazon_Angular/src/app/pages/szerviz/szerviz.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-szerviz',
+  imports: [],
+  templateUrl: './szerviz.html',
+  styleUrl: './szerviz.css',
+})
+export class Szerviz {
+
+}
