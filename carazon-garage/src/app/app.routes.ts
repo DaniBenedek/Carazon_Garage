fa3874c0f5a417072dd9959fe4be13a0baa83c20
DaniBenedek <dbenedek06@gmail.com>
2025-11-13 11:05:06 +0100

    Névimportok

--- carazon-garage/src/app/app.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router';
-import { Szerviz } from './pages/szerviz/szerviz';
-import { Home } from './pages/home/home';
-
-export const routes: Routes = [
-  { path: 'Szerviz', component: Szerviz },
-  { path: '', component: Home }
-];
