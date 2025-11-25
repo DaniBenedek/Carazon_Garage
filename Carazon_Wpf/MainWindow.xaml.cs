72700d55a8bc593e63f5d99ec388ada3fa630014
DaniBenedek <dbenedek06@gmail.com>
2025-11-25 08:44:54 +0100

    wpf kinezet

--- Carazon_Wpf/MainWindow.xaml.cs
+++ Carazon_Wpf/MainWindow.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using MySqlConnector;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +23,91 @@ namespace carazonGarage
     /// </summary>
     public partial class MainWindow : Window
     {
+        MySql.Data.MySqlClient.MySqlConnection connection = new MySql.Data.MySqlClient.MySqlConnection("server=localhost;database=hidak;uid=root");
+        MySql.Data.MySqlClient.MySqlCommand command;
         public MainWindow()
         {
             InitializeComponent();
+            AdatbazisIndit();
+        }
+        public void openConnection()
+        {
+            if (connection.State == ConnectionState.Closed)
+            {
+                connection.Open();
+            }
+        }
+        private void AdatbazisIndit()
+        {
+            try
+            {
+                MySql.Data.MySqlClient.MySqlDataAdapter adapter = new MySql.Data.MySqlClient.MySqlDataAdapter("SELECT * FROM fuggohidak", connection);
+                openConnection();
+                DataSet ds = new DataSet();
+                adapter.Fill(ds);
+                Datagrid_Szerviz.ItemsSource = ds.Tables[0].DefaultView;
+                closeConnection();
+            }
+            catch (Exception hiba)
+            {
+                MessageBox.Show(hiba.Message);
+            }
+        }
+
+        public void executeQuery(string query)
+        {
+            try
+            {
+                openConnection();
+                command = new MySql.Data.MySqlClient.MySqlCommand(query, connection);
+                if (command.ExecuteNonQuery() >= 1)
+                {
+                    MessageBox.Show("Végrehajtva!");
+                }
+                else
+                {
+                    MessageBox.Show("Nem lett végrehajtva!");
+                }
+            }
+            catch (Exception kivetel)
+            {
+                MessageBox.Show(kivetel.Message);
+            }
+            finally
+            {
+                closeConnection();
+            }
+        }
+
+        public void closeConnection()
+        {
+            if (connection.State == ConnectionState.Open)
+            {
+                connection.Close();
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            ugyfelek mainWindow = new ugyfelek();
+            this.Hide();
+            mainWindow.Show();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void Datagrid_Szerviz_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
         }
     }
 }
+
