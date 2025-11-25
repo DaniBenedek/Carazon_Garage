a2af26424111a504d6b558ddd9998b79e6d52f37
DaniBenedek <dbenedek06@gmail.com>
2025-11-25 07:41:27 +0100

    login gomb

--- Carazon_Angular/src/app/app.routes.ts
+++ Carazon_Angular/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { Szerviz } from './pages/szerviz/szerviz';
+import { Login } from './pages/login/login';
 import { Home } from './pages/home/home';
 
 export const routes: Routes = [
   { path: 'Szerviz', component: Szerviz },
+  { path: 'Login', component: Login },
   { path: '', component: Home }
 ];
