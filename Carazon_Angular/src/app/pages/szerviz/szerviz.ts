ca45561f4206cdeb8f600133736e8f707887ddc2
DaniBenedek <dbenedek06@gmail.com>
2025-12-16 17:14:54 +0100

    szerviz hyperlink fix

--- Carazon_Angular/src/app/pages/szerviz/szerviz.ts
+++ Carazon_Angular/src/app/pages/szerviz/szerviz.ts
@@ -1,31 +1,12 @@
 import { Component } from '@angular/core';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
-import { API_URL } from '../../app.config';  // útvonalat igazítsd!
 
 @Component({
   selector: 'app-szerviz',
   standalone: true,
-  imports: [HttpClientModule],
+  imports: [],
   templateUrl: './szerviz.html',
   styleUrl: './szerviz.css',
 })
 export class Szerviz {
   
-  cars: any[] = [];
-
-  constructor(private http: HttpClient) {
-    this.loadCars();
-  }
-
-  loadCars() {
-    this.http.get(`${API_URL}/api/cars`).subscribe({
-      next: (data: any) => {
-        this.cars = data;
-        console.log("Lekért autók:", data);
-      },
-      error: err => {
-        console.error("API hiba:", err);
-      }
-    });
-  }
 }
