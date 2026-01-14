392e8117c6d6955c35d50779cbf965ac298ded1b
MiklosMartin <miklos.martin-2021@keri.mako.hu>
2026-01-14 13:31:10 +0100

    Register létrehozása, betagolása, Register.ts megcsinálása majd a login betagolása

--- Carazon_Angular/src/app/pages/register/register.ts
+++ Carazon_Angular/src/app/pages/register/register.ts
@@ -1,16 +1,34 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common'; // Az *ngIf miatt kell
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  standalone: true,
+  imports: [FormsModule, CommonModule], // Mindkettő kell ide
   templateUrl: './register.html',
   styleUrl: './register.css',
 })
 export class Register {
+  regData = {
+    name: '',
+    email: '',
+    phone: '',
+    password: '',
+    confirmPassword: ''
+  };
 
   isPasswordVisible: boolean = false;
   
   togglePassword() {
     this.isPasswordVisible = !this.isPasswordVisible;
+  }
+
+  onSubmit() {
+    if (this.regData.password === this.regData.confirmPassword) {
+      console.log('Sikeres regisztráció adatai:', this.regData);
+    } else {
+      console.error('Hiba: A jelszavak nem egyeznek!');
+    }
   }
 }
