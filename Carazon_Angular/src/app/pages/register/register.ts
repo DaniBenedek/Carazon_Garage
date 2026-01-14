decee1d3c6387d6d97432aae216634c7e3694fa2
MiklosMartin <miklos.martin-2021@keri.mako.hu>
2026-01-14 14:02:10 +0100

    placeholder probléma megoldása és az email @ kötelezővé tétele

--- Carazon_Angular/src/app/pages/register/register.ts
+++ Carazon_Angular/src/app/pages/register/register.ts
@@ -1,15 +1,19 @@
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { CommonModule } from '@angular/common'; // Az *ngIf miatt kell
+import { FormsModule } from '@angular/forms'; // Kell a form validációhoz és az ngModel-hez
+import { CommonModule } from '@angular/common'; // Kell az *ngIf hibaüzenethez
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [FormsModule, CommonModule], // Mindkettő kell ide
+  imports: [
+    FormsModule, 
+    CommonModule
+  ],
   templateUrl: './register.html',
   styleUrl: './register.css',
 })
 export class Register {
+  // Ebben az objektumban tároljuk az űrlap adatait
   regData = {
     name: '',
     email: '',
@@ -24,11 +28,13 @@ export class Register {
     this.isPasswordVisible = !this.isPasswordVisible;
   }
 
+  // Ez a függvény fut le a Regisztráció gomb megnyomásakor
   onSubmit() {
-    if (this.regData.password === this.regData.confirmPassword) {
-      console.log('Sikeres regisztráció adatai:', this.regData);
-    } else {
-      console.error('Hiba: A jelszavak nem egyeznek!');
-    }
+    // Itt íratjuk ki az adatokat a konzolra
+    console.log('Sikeres regisztráció! Az adatok:');
+    console.log('Név:', this.regData.name);
+    console.log('Email:', this.regData.email);
+    console.log('Telefon:', this.regData.phone);
+    console.log('Jelszó:', this.regData.password);
   }
 }
