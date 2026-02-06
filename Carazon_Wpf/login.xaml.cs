13a8206c5dc8834c91837dee69ef0da773ec8b44
MiklosMartin <miklos.martin-2021@keri.mako.hu>
2026-02-06 10:54:58 +0100

    login x kilépés

--- Carazon_Wpf/login.xaml.cs
+++ Carazon_Wpf/login.xaml.cs
@@ -56,5 +56,11 @@ namespace carazonGarage
                 connection.Close();
             }
         }
+
+        private void Kilepes_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            this.Close();
+        }
     }
 }
